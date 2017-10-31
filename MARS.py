eb670fb163e09242449b2128ddc859477f5a41d5
Bendik Witzoe <bendikw@gmail.com>
2017-10-31 17:45:14 +0100

    fixed mars plot

--- MARS.py
+++ MARS.py
@@ -10,7 +10,7 @@ data_file = "welltests.csv"
 
 class Mars:
 
-    def __init__(self, miss = True, prune=False, max_terms=6, penalty=0.1, minspan=3):
+    def __init__(self, miss = True, prune=False, max_terms=30, penalty=0.0, minspan=3):
         self.model = Earth(allow_missing=miss, enable_pruning=prune, max_terms=max_terms, penalty=penalty,
                   minspan=minspan)
         dframe = pd.read_csv(data_file, sep=",")
@@ -20,8 +20,10 @@ class Mars:
         
     def plot_fig(self, X, y, y_hat, title="Model fit"):
         pyplot.figure()
-        pyplot.plot(X,y,'r.')
-        pyplot.plot(X,y_hat,'b.')
+##        X = [d[0] for d in data]
+####        y = [d[1] for d in data]
+        pyplot.plot(X,y,'b.')
+        pyplot.plot(X,y_hat,'r')
         pyplot.xlabel('gaslift')
         pyplot.ylabel('oil')
         pyplot.title(title)
@@ -31,14 +33,21 @@ class Mars:
 ##        self.model = Earth(allow_missing=True, enable_pruning=False, max_terms=6, penalty=0.1,
 ##                  minspan=6)
         df_w = self.df.loc[self.df['well'] == well]
-        X,y = cl.gen_targets(self.df, well, normalize=True, intervals=100)
-        X = np.array(X)
-        y = np.array(y)
+##        X,y = cl.gen_targets(self.df, well, normalize=True)
+        data = cl.conv_to_batch([cl.gen_targets(self.df, well, normalize=False, intervals=100)])
+        data.sort()
+####        data_sorted = cl.conv_to_batch([X,y]).sort()
+        X = [d[0] for d in data]
+        y = [d[1] for d in data]
         self.model.fit(X, y)
     ##    print(model.trace())
         print(self.model.summary())
         y_hat = self.model.predict(X)
+        print(y_hat)
+        X = [x[0] for x in X]
+        print("\n", X, "\n\n")
         self.plot_fig(X, y, y_hat, well)
+
 
 def run_all():
     m = Mars()
