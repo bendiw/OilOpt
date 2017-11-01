83c76b376dc6b2df6501f40949d8ba1d3ebf0b55
arntgm <agmalvik@gmail.com>
2017-11-01 10:26:18 +0100

    breakpoints

--- tens.py
+++ tens.py
@@ -32,13 +32,12 @@ def total_batch(data):
 
 def plot_pred(x, pred, y):
     pyplot.figure()
-    pyplot.plot(x,pred,'r')
+    bp = pyplot.plot(x,pred,'r')
     pyplot.plot(x,y,'b.')
-    pyplot.show()
+    return bp
 
-##df = cl.load("C:\\Users\\agmal_000\\Skole\\Optimering fordypning\\OilOpt\\OilOpt\\welltests.csv")
-##df = cl.load("C:\\Users\\Bendik\\Documents\\GitHub\\OilOpt\\welltests.csv")
-data = [cl.gen_targets(df, "A5", normalize=True)] #,intervals=100
+df = cl.load("welltests.csv")
+data = [cl.gen_targets(df, "C3", normalize=True, intervals = 100)] #,intervals=100
 data = cl.conv_to_batch(data)
 data.sort()
 ##print(len(data))
@@ -49,7 +48,7 @@ data.sort()
 x = tf.placeholder(tf.float64, [None,1])
 y_ = tf.placeholder(tf.float64, [None,1])
 
-n_hidden = 5
+n_hidden = 100
 
 L1, W1, b1 = add_layer(x, 1, n_hidden, activation_function = None)
 L2, W2, b2 = add_layer(x, 1, n_hidden, activation_function = None)
@@ -58,14 +57,14 @@ out1 = max_out(L1, 1)
 out2 = max_out(L2, 1)
 out = tf.subtract(out1,out2)
 
-error = tf.reduce_sum((tf.square(y_ - out)))
+error = tf.reduce_mean((tf.square(y_ - out)))
 ##error = tf.losses.sigmoid_cross_entropy()
 
-train_step = tf.train.AdamOptimizer(0.0001).minimize(error)
+train_step = tf.train.AdamOptimizer(0.01).minimize(error)
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 
-for i in range(25000 + 1):
+for i in range(5000 + 1):
     batch_xs, batch_ys = next_batch(data, len(data))
 ##    print (batch_xs, batch_ys)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
@@ -73,12 +72,35 @@ for i in range(25000 + 1):
         total_x, total_y = next_batch(data, len(data))
         res = sess.run(error, feed_dict={x: total_x, y_: total_y})
         print ("Step %04d" %i, " error = %g" %res)
-        if(res<0.001):
+        if(res<0.05):
             break
 
 total_x, total_y = total_batch(data)
 pred = sess.run(out, feed_dict={x: total_x, y_: total_y})
-plot_pred(total_x, pred, total_y)
+bp = plot_pred(total_x, pred, total_y)
+
+xvalues = bp[0].get_xdata()
+yvalues = bp[0].get_ydata()
+breakpoints_y = [yvalues[0]]
+breakpoints_x = [xvalues[0]]
+old_w = (yvalues[1]-yvalues[0])/(xvalues[1]-xvalues[0])
+for i in range(2,len(yvalues)-1):
+    w = (yvalues[i]-yvalues[i-1])/(xvalues[i]-xvalues[i-1])
+    print ("")
+    print ("w:",w)
+    print ("old_w:",old_w)
+    if (abs(w-old_w)>0.00001):
+        breakpoints_y.append(yvalues[i-1])
+        breakpoints_x.append(xvalues[i-1])
+    old_w = w
+breakpoints_y.append(yvalues[-1])
+breakpoints_x.append(xvalues[-1])
+
+print (breakpoints_x)
+print (breakpoints_y)
+pyplot.plot(breakpoints_x, breakpoints_y, 'k*', markersize = 6.0)
+sess.close()
+pyplot.show()
 ##weights, biases = sess.run(W), sess.run(b)
 ##print (weights)
 ##print (biases)
