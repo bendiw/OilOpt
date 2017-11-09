7739516ed34f91da94dc706d60682cf39a863cca
Bendik Witzoe <bendikw@gmail.com>
2017-11-09 12:18:51 +0100

    surface plots

--- plotter.py
+++ plotter.py
@@ -1,29 +1,28 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 import numpy as np
 
 def plot3d(x, y, z):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+    x = np.array(x)
+    y = np.array(y)
+    z = np.array(z)
+    print(x.shape, y.shape, z.shape)
 
-##    x = [0,1,2]
-##    y = [2,1,0]
-##    z = [1, 1.5, 2]
-    ax.scatter(x, y, z)
+##    x = [0,1,2,1,1,1]
+##    y = [2,1,0,5,4,2]
+##    z = [1, 1.5, 2,5,3,4]
+    ax.set_xlabel('gas lift')
+    ax.set_ylabel('choke')
+    ax.set_zlabel('output')
+#    ax.scatter(x, y, z)
+    ax.plot_trisurf(x, y, z, linewidth=0.2, antialiased=True)
+
+##    surf = ax.plot_surface(x,y,z, cmap=cm.coolwarm,
+##                       linewidth=0, antialiased=False)
     plt.show()
 
-def test3d(d_dict):
-    t_x = []
-    t_y = []
-    t_z = []
-    x, y, z = d_dict['gaslift'], d_dict['choke'], d_dict['output']
-##        print(x, y, z)
-    x_v = np.arange(np.nanmin(x), np.nanmax(x), (np.nanmax(x)-np.nanmin(x))/20)
-    y_v = np.arange(np.nanmin(y), np.nanmax(y), (np.nanmax(y)-np.nanmin(y))/20)
-    print(np.nanmax(y))
-    for i in x_v:
-        for j in y_v:
-            t_x.append(i)
-            t_y.append(j)
-            t_z.append(self.model.predict([[i, j]]))
-    plot3d(t_x, t_y, t_z)
+#plot3d(0,0,0)
+
