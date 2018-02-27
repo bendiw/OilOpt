2e116c43c1ff6a1dfbacfff62cf710685c3361a9
arntgm <agmalvik@gmail.com>
2018-02-27 13:59:55 +0100

    asd

--- tens_relu.py
+++ tens_relu.py
@@ -19,15 +19,15 @@ import tens
 # when it is provided by the caller. input_size and output_size is the number of neurons
 #in the previos layer and in the current layer, respectively
 # =============================================================================
-def add_layer(inputs, input_size, output_size, trainable = True, w_lock = False, activation_function = None, relu_unit="0"):
+def add_layer(inputs, input_size, output_size, bias = True, activation_function = None, relu_unit="0"):
     weightname = "weight-"+relu_unit
     biasname = "bias-"+relu_unit
-    if not (w_lock):
-        W = tf.Variable(np.random.uniform(-1, 1, size = (input_size, output_size)), trainable = trainable, name=weightname)
-        b = tf.Variable(np.random.uniform(-1, 1, size =output_size), trainable = trainable,name=biasname)
+    if (bias == True):
+        W = tf.Variable(np.random.uniform(-1, 1, size = (input_size, output_size)), name=weightname)
+        b = tf.Variable(np.random.uniform(-10000, 10000, size =output_size), name=biasname)
         output = tf.matmul(inputs, W) + b
     else:
-        W = tf.Variable(np.ones((input_size,output_size)), trainable = trainable, name=weightname)
+        W = tf.Variable(np.ones((input_size, output_size)), name=weightname)
         output = tf.matmul(inputs, W)
         b=None
     if activation_function is not None:
@@ -35,39 +35,42 @@ def add_layer(inputs, input_size, output_size, trainable = True, w_lock = False,
     print("New layer with activation function", activation_function)
     return output, W, b
 
+def leaky_relu(x):
+    return tf.nn.relu(x) - 0.3*tf.nn.relu(-x)
+
 # =============================================================================
 #  Initiates a new TF network if an old one is not provided. If an old one is provided, the
 # weights and biases are reinitialized. Sends prev back and forth between methods. Prev has
 #    all the necessary TF session information
 # =============================================================================
-def hey(datafile, test_error = False, goal='oil', grid_size = 8, plot = False,learning_rate=0.03, factor = 1.5, cross_validation = None,
-        epochs = 500, beta = 0.2, val_interval = None, train_frac = 1.0, val_frac = 0.0, n_hidden = 30,
-        k_prob = 1.0, normalize = True, intervals = 20, nan_ratio = 0.3, hp=1, prev = None, save = False):
+def hey(datafile, test_error = False, goal='oil', grid_size = 8, plot = False,learning_rate=0.001, factor = 1.5, cross_validation = None,
+        epochs = 15000, beta = 0.01, val_interval = None, train_frac = 1.0, val_frac = 0.0, n_hidden = 15,
+        k_prob = 1.0, normalize = False, intervals = 20, nan_ratio = 0.3, hp=1, prev = None, save = False):
     if (prev == None):
         prev = [0]*2
         x = tf.placeholder(tf.float64, [None,1])
         y_ = tf.placeholder(tf.float64, [None,1])
         keep_prob = tf.placeholder(tf.float64)
-        L1, W1, b1 = add_layer(x, 1, 20, activation_function = tf.nn.relu, relu_unit="1-1")
-        L12, W12, b12 = add_layer(L1, 20, 1, w_lock=1, trainable=False, relu_unit="1-2")
+        L1, W1, b1 = add_layer(x, 1, n_hidden, activation_function = leaky_relu, relu_unit="1-1")
         regularizers = tf.nn.l2_loss(W1)
-        dropout1 = tf.nn.dropout(L12, keep_prob)
-        loss = tf.reduce_mean(tf.square(y_ - dropout1) + beta * regularizers)
+        L12, W12, b12 = add_layer(L1, n_hidden, 1, activation_function = None, bias=True, relu_unit="1-2")
+        out1 = tf.nn.dropout(L12, keep_prob)
+        loss = tf.reduce_mean(tf.square(y_ - out1) + beta * regularizers)
         train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
         sess = tf.InteractiveSession()
-        prev[0] = [sess, train_step, x, y_, keep_prob, dropout1, loss]
+        prev[0] = [sess, train_step, x, y_, keep_prob, out1, loss]
         print("Initializing Tensorflow modules. This may take a while...")
         
         x2 = tf.placeholder(tf.float64, [None,2])
         y_2 = tf.placeholder(tf.float64, [None,1])
         keep_prob2 = tf.placeholder(tf.float64)
-        L2, W2, b2 = add_layer(x2, 2, n_hidden, activation_function = tf.nn.relu, relu_unit="2-1")
-        L22, W22, b22 = add_layer(L2, n_hidden, 1, w_lock=1, trainable=False, relu_unit="2-2")
+        L2, W2, b2 = add_layer(x2, 2, n_hidden, activation_function = leaky_relu, relu_unit="2-1")
         regularizers2 = tf.nn.l2_loss(W2)
-        dropout2 = tf.nn.dropout(L22, keep_prob2)
-        loss2 = tf.reduce_mean(tf.square(y_2 - dropout2) + beta * regularizers2)
+        L22, W22, b22 = add_layer(L2, n_hidden, 1, activation_function = None, bias=True, relu_unit="2-2")
+        out2 = tf.nn.dropout(L22, keep_prob2)
+        loss2 = tf.reduce_mean(tf.square(y_2 - out2) + beta * regularizers2)
         train_step2 = tf.train.AdamOptimizer(learning_rate).minimize(loss2)
-        prev[1] = [sess, train_step2, x2, y_2, keep_prob2, dropout2, loss2]
+        prev[1] = [sess, train_step2, x2, y_2, keep_prob2, out2, loss2]
     tf.global_variables_initializer().run()
     return run(datafile, prev, goal=goal, test_error=test_error,
                hp=hp, grid_size=grid_size, plot=plot, epochs=epochs, beta=beta, val_interval = val_interval, train_frac=train_frac,
@@ -78,10 +81,10 @@ def hey(datafile, test_error = False, goal='oil', grid_size = 8, plot = False,le
 # Runs through a series of training runs
 # =============================================================================
 def run(datafile, prev, test_error=False, goal='oil', grid_size = 8, plot = False, factor = 1.5, cross_validation = None,
-        epochs = 500, beta = 0.1,  val_interval = None, train_frac = 1.0, val_frac = 0.0, n_hidden = 5,
+        epochs = 1000, beta = 0.01,  val_interval = None, train_frac = 1.0, val_frac = 0.0, n_hidden = 10,
         k_prob = 1.0, normalize = True, intervals = 20, nan_ratio = 0.3, hp=1, save = False):
     df = cl.load("welltests_new.csv")
-    dict_data, means, stds = cl.gen_targets(df, datafile+"", goal=goal, normalize=True, intervals=intervals,
+    dict_data, means, stds = cl.gen_targets(df, datafile+"", goal=goal, normalize=normalize, intervals=intervals,
                                factor = factor, nan_ratio = nan_ratio, hp=hp) #,intervals=100
     data = tens.convert_from_dict_to_tflists(dict_data)
     is_3d = False
@@ -92,7 +95,8 @@ def run(datafile, prev, test_error=False, goal='oil', grid_size = 8, plot = Fals
     else:
         sess, train_step, x, y_, keep_prob, out, regloss = prev[0][0], prev[0][1], prev[0][2], prev[0][3], prev[0][4], prev[0][5], prev[0][6]
         print("Well",datafile, goal, "- Gaslift only")
-    loss = tf.reduce_mean(tf.abs(y_ - out))
+    loss = regloss
+#    loss = tf.reduce_mean(tf.abs(y_ - out))
     all_data_points = data.copy()
     val_loss = []
     train_loss = []
@@ -101,7 +105,8 @@ def run(datafile, prev, test_error=False, goal='oil', grid_size = 8, plot = Fals
     if (cross_validation == None):
         train_set, validation_set, test_set = tens.generate_sets(data, train_frac, val_frac)
         for i in range(epochs + 1):
-            batch_xs, batch_ys = tens.next_batch(train_set, len(train_set))
+            batch_xs, batch_ys = tens.total_batch(train_set)
+#            print (batch_xs, batch_ys)
             sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: k_prob})
             if (val_interval != None and val_frac > 0 and i%val_interval == 0):
                 val_xs, val_ys = tens.next_batch(validation_set, len(validation_set))
@@ -110,6 +115,8 @@ def run(datafile, prev, test_error=False, goal='oil', grid_size = 8, plot = Fals
                 val_loss.append(vloss)
                 tloss = sess.run(loss, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 1.0})
                 train_loss.append(tloss)
+                print ("Step %04d" %i, " validation loss = %g" %vloss, "  training loss = %g" %tloss)
+
 
         
     if (test_error):
@@ -122,15 +129,19 @@ def run(datafile, prev, test_error=False, goal='oil', grid_size = 8, plot = Fals
         return error, prev, means, stds
         
     total_x, total_y = tens.total_batch(all_data_points)
+    print(total_x)
+    print(total_y)
     pred = sess.run(out, feed_dict={x: total_x, y_: total_y, keep_prob: 1.0})
     R2 = tens.r2(total_y, pred)
     print("R2 =",R2)
+    print("Predictions:", pred)
     breakpoints = 0
     if (is_3d):
         x_vals = tens.get_x_vals(dict_data, grid_size)
         y_vals = [[0] for i in range(len(x_vals))]
         pred = sess.run(out, feed_dict={x: x_vals, y_: y_vals, keep_prob: 1.0})
-        x_vals, y_vals, pred = tens.denormalize(x_vals, y_vals, pred, means, stds)
+        if (normalize):
+            x_vals, y_vals, pred = tens.denormalize(x_vals, y_vals, pred, means, stds)
         x1 = [x[0] for x in x_vals]
         x2 = [x[1] for x in x_vals]
         z = []
@@ -141,7 +152,8 @@ def run(datafile, prev, test_error=False, goal='oil', grid_size = 8, plot = Fals
         breakpoints = tools.delaunay(x1,x2,z)        
         
     else:
-        total_x, total_y, pred = tens.denormalize(total_x, total_y, pred, means, stds)
+        if (normalize):
+            total_x, total_y, pred = tens.denormalize(total_x, total_y, pred, means, stds)
         xvalues, yvalues = [], []
         for i in range(len(total_x)):
             xvalues.append(total_x[i][0])
@@ -179,11 +191,21 @@ def run(datafile, prev, test_error=False, goal='oil', grid_size = 8, plot = Fals
         tens.save_variables(datafile, hp, goal, is_3d)
     return is_3d, breakpoints, prev, total_x, pred, total_y, R2, dict_data, means, stds, train_loss, val_loss
     
+
     
-
-
-
-
-
-
-        
+def load(well, phase, separator ):
+    filename = "" + well + "-" + separator + "-" + phase + ".txt"
+    with open(filename) as f:
+        content = f.readlines()
+    content = [x.strip() for x in content]
+    print (content)
+    dim = int(content[0])
+    w = []
+    b = []
+    for k in range(1,3):
+        if(dim == 1):
+            w.append([float(x) for x in content[k].split()])
+            b.append([float(x) for x in content[k+2].split()])
+        else:
+            return
+    return dim, w, b
