7557ee6c34418c9ddbece3a121b7d717ca897232
arntgm <agmalvik@gmail.com>
2018-03-01 11:52:55 +0100

    asd

--- tens_relu.py
+++ tens_relu.py
@@ -200,7 +200,7 @@ def load(well, phase, separator ):
     with open(filename) as f:
         content = f.readlines()
     content = [x.strip() for x in content]
-
+    
 #    print (content)
 
     dim = int(content[0])
