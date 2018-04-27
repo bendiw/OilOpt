8f45e6ede561ac625c1badeffe8390e0dd5d8e32
arntgm <arntgm@users.noreply.github.com>
2018-04-27 13:24:02 +0200

    asd

--- gridsearch.py
+++ gridsearch.py
@@ -126,7 +126,7 @@ def create_model(tau=0.005, length_scale=0.001, dropout=0.05, score="ll",
 
 def search(well, separator="HP", case=1, parameters=t.param_dict, variance="heterosced", x_grid=None, y_grid=None, verbose=2, nan_ratio=0.0, goal='oil'):
     if(well):
-        X, y = cl.BO_load(well, separator, case=case, nan_ratio=nan_ratio, goal=goal)
+        X, y,_ = cl.BO_load(well, separator, case=case, nan_ratio=nan_ratio, goal=goal)
 
         if(x_grid is not None):
             print("Datapoints before merge:",len(X))
