4146a27fd799ba0aac59a125f93e40f197081d4d
bendiw <bendikw@gmail.com>
2018-05-13 20:30:33 +0200

    changed recourse

--- scen_gen_robust_v2.py
+++ scen_gen_robust_v2.py
@@ -72,12 +72,13 @@ class NN:
     def evaluate_recourse(self, iterations=201, num_scen=100, init_name="zero", max_changes=3, verbose=0):
         scen_draws = t.get_scenario(case=2, num_scen=iterations, distr="truncnorm")
         results = np.zeros((iterations,2))
-#        print(scen_draws)
+        
+        #get basic solution
+        base_sol = self.recourse_opt(init_name=init_name, max_changes=max_changes, num_scen=num_scen, pregen_scen=scen_draws.loc[0].to_dict(), verbose=verbose, base_solution=None)
+        #now iterate with max_changes-1 since basic solution used max_changes
         for i in range(iterations):
             print("iteration:", i)
-#            print(results)
-#            print(scen_draws.loc[i])
-            results[i] = self.recourse_opt(init_name=init_name, max_changes=max_changes, num_scen=num_scen, pregen_scen=scen_draws.loc[i].to_dict(), verbose=verbose)
+            results[i] = self.recourse_opt(init_name=init_name, max_changes=max_changes-1, num_scen=num_scen, pregen_scen=scen_draws.loc[i].to_dict(), verbose=verbose, base_solution=base_sol)
         res_df = pd.DataFrame(results, columns=["infeasible_count", "total_oil"])
         print(res_df)
         res_df.to_csv("results/robust_recourse_iterative/results_"+init_name+str(num_scen)+"_scen_"+str(iterations)+"_iter.csv", sep=';', header=True)
@@ -87,43 +88,41 @@ class NN:
     # following the first step of an optimization result possibly involving
     # several wells. The scenario is drawn and locked for the implemented well.
     # We then re-optimize.
+    #
+    # The base_solution flag is used to obtain a first solution since it will
+    # be equal for all pregen_scen
     # =============================================================================
-    def recourse_opt(self, init_name, max_changes, num_scen, pregen_scen=None, verbose=1):
+    def recourse_opt(self, init_name, max_changes, num_scen, pregen_scen=None, verbose=1, base_solution=None):
 #        results = pd.DataFrame(columns=t.robust_res_columns) 
         scen_draws = t.get_scenario(case=2, num_scen=10000, distr="truncnorm")
         scen_const = {}
         lock_wells = []
-        #get starting point
-        results= t.get_robust_solution(init_name=init_name)
-        current_contrib_gas = {w:results[w+"_gas_mean"].values[0]+pregen_scen[w]*results[w+"_gas_var"].values[0] for w in t.wellnames_2}
-        current_contrib_oil = {w:results[w+"_oil_mean"].values[0] for w in t.wellnames_2}
         implemented = t.get_robust_solution(init_name=init_name)
-        results.columns = t.robust_res_columns_recourse
         implemented.columns = t.robust_res_columns_recourse
 #        print("oil contrib:", current_contrib_oil)
 #        print("gas contrib:", current_contrib_gas)
 
         change_well = None
         chokenames = [w+"_choke" for w in t.wellnames_2]
-        init_choke = results.loc[0][chokenames].to_dict()
         infeasible_count= 0
         infeasible = False
-        for i in range(max_changes, 0, -1):
-            iter_num = max_changes+1-i
-            if(change_well and not infeasible):
-                #keep track of choke settings
-                init_choke[change_well+"_choke"] = results.loc[iter_num-1][change_well+"_choke"]
-
-            #optimize
-            res = self.init(num_scen=num_scen, save=False, init_name=init_choke, max_changes=i, recourse_iter=True, 
-                            scen_const=scen_const, verbose=0)
-            results.loc[iter_num] = res
+        #get starting point
+        results= t.get_robust_solution(init_name=init_name)
+        results.columns = t.robust_res_columns_recourse
+        current_contrib_gas = {w:results[w+"_gas_mean"].values[0]+pregen_scen[w]*results[w+"_gas_var"].values[0] for w in t.wellnames_2}
+        current_contrib_oil = {w:results[w+"_oil_mean"].values[0] for w in t.wellnames_2}
+        init_choke = results.loc[0][chokenames].to_dict()
+        if base_solution is not None:
+            #solution was given as argument
+            #we now check if it is feasible given the pregen_scen
             z = np.zeros((1,implemented.shape[1]))
-            implemented.loc[iter_num] = z[0]
+            implemented.loc[implemented.shape[0]] = z[0]
             
             #greedily implement 1 change from solution
-            for w in t.well_order:
-                if(results.loc[iter_num][w+"_changed"]>0 and results.loc[iter_num][w+"_choke"]-init_choke[w+"_choke"]>0.01): # and results.loc[iter_num]
+            found = False
+            #start by decrementing in reverse order
+            for w in t.well_order[::-1]:
+                if(base_solution[w+"_changed"]>0 and round(base_solution[w+"_choke"], 4)-round(init_choke[w+"_choke"],4)<0):
                     if(w not in lock_wells):
                         lock_wells.append(w)
                         if not pregen_scen:
@@ -132,7 +131,86 @@ class NN:
                         else:
                             scen_const[w] = pregen_scen[w]
                     change_well = w
+                    found = True
                     break
+            if not found:
+                for w in t.well_order:
+                    if(base_solution[w+"_changed"]>0 and base_solution[w+"_choke"]-init_choke[w+"_choke"]>0.01): # and results.loc[iter_num]
+                        if(w not in lock_wells):
+                            lock_wells.append(w)
+                            if not pregen_scen:
+                                s = np.random.randint(0, 10000)
+                                scen_const[w] = scen_draws.loc[s][w]
+                            else:
+                                scen_const[w] = pregen_scen[w]
+                        change_well = w
+                        break
+            new_indiv_gas = base_solution[change_well+"_gas_mean"]+scen_const[change_well]*base_solution[change_well+"_gas_var"]
+            new_indiv_oil = base_solution[change_well+"_oil_mean"]
+            new_tot_gas = implemented.loc[0]["tot_gas"]+new_indiv_gas - current_contrib_gas[change_well]
+            infeasible = (round(new_indiv_gas, 2) > results.loc[0]["indiv_cap"] or new_tot_gas>results.loc[0]["tot_cap"])
+
+            if(infeasible):
+                #revert solution
+                implemented.loc[implemented.shape[0]] = implemented.loc[0]
+                results.loc[results.shape[0]-1] = results.loc[0]
+                infeasible_count+=1
+            else:
+                current_contrib_gas[change_well] = new_indiv_gas
+                results.loc[results.shape[0]] = base_solution
+                implemented.loc[implemented.shape[0]-1]["tot_gas"] = new_tot_gas
+                current_contrib_oil[change_well] = new_indiv_oil
+                implemented.loc[implemented.shape[0]-1]["tot_oil"] = sum(current_contrib_oil.values())
+                implemented.loc[implemented.shape[0]-1][change_well+"_choke"] = base_solution[change_well+"_choke"]
+                implemented.loc[implemented.shape[0]-1][change_well+"_gas_mean"] = new_indiv_gas
+                init_choke[change_well+"_choke"] = base_solution[change_well+"_choke"]
+
+                
+#            print("impl pre-iteration start:\n", implemented)
+        #iterate down to 1 change allowed
+        for i in range(max_changes, 0, -1):
+            iter_num = max_changes+2-i
+            if(change_well and not infeasible):
+                #keep track of choke settings
+                init_choke[change_well+"_choke"] = results.loc[iter_num-1][change_well+"_choke"]
+
+            #optimize
+            res = self.init(num_scen=num_scen, save=False, init_name=init_choke, max_changes=i, recourse_iter=True, 
+                            scen_const=scen_const, verbose=0)
+            results.loc[iter_num] = res
+            if base_solution is None:
+                #return base solution
+                return results.loc[iter_num]
+            z = np.zeros((1,implemented.shape[1]))
+            implemented.loc[iter_num] = z[0]
+            
+            #greedily implement 1 change from solution
+            found = False
+            #start by decrementing in reverse order
+            for w in t.well_order[::-1]:
+                if(results.loc[iter_num][w+"_changed"]>0 and round(results.loc[iter_num][w+"_choke"], 4)-round(init_choke[w+"_choke"],4)<0):
+                    if(w not in lock_wells):
+                        lock_wells.append(w)
+                        if not pregen_scen:
+                            s = np.random.randint(0, 10000)
+                            scen_const[w] = scen_draws.loc[s][w]
+                        else:
+                            scen_const[w] = pregen_scen[w]
+                    change_well = w
+                    found = True
+                    break
+            if not found:
+                for w in t.well_order:
+                    if(results.loc[iter_num][w+"_changed"]>0 and results.loc[iter_num][w+"_choke"]-init_choke[w+"_choke"]>0.01): # and results.loc[iter_num]
+                        if(w not in lock_wells):
+                            lock_wells.append(w)
+                            if not pregen_scen:
+                                s = np.random.randint(0, 10000)
+                                scen_const[w] = scen_draws.loc[s][w]
+                            else:
+                                scen_const[w] = pregen_scen[w]
+                        change_well = w
+                        break
             new_indiv_gas = results.loc[iter_num][change_well+"_gas_mean"]+scen_const[change_well]*results.loc[iter_num][change_well+"_gas_var"]
             new_indiv_oil = results.loc[iter_num][change_well+"_oil_mean"]
             new_tot_gas = implemented.loc[iter_num-1]["tot_gas"]+new_indiv_gas - current_contrib_gas[change_well]
@@ -152,14 +230,17 @@ class NN:
                 implemented.loc[iter_num][change_well+"_gas_mean"] = new_indiv_gas
             if(verbose>0):
                 print("\n\niteration", iter_num)
+#                for w in chokenames:
+#                    print(w, "change:", round(results.loc[iter_num][w], 4)-round(init_choke[w],4))
                 print("initial choke settings:", init_choke)
                 print("scenario draws:", scen_const)
-                print("well to change:", change_well)
+                print("well to change:", change_well, "-->", results.loc[iter_num][change_well+"_choke"])
                 print("indiv gas:", new_indiv_gas, "\ntot_gas", new_tot_gas, "\ninfeasible?", infeasible)
         if(verbose>0):
-            for c in results.columns:
-                print(results[c])
             print("\n\nImplemented:\n", implemented[implemented.columns[2:11]])
+            if(verbose>1):
+                for c in results.columns:
+                    print(results[c])
         return [infeasible_count, implemented.loc[implemented.shape[0]-1]["tot_oil"]]
     
     # =============================================================================
@@ -358,7 +439,9 @@ class NN:
         self.m.addConstrs( (routes[well, sep] == 0) >> (outputs_oil[well, sep] == 0) for well in self.wellnames for sep in self.well_to_sep[well] )
         
         
-        
+        #input forcing to track changes
+        self.m.addConstrs( (routes[well, sep] == 0) >> (inputs[well, sep, dim] <= 0) for well in self.wellnames for sep in self.well_to_sep[well] for dim in range(self.multidims[well]["oil"][sep][0]))
+
         
         
 
@@ -414,8 +497,10 @@ class NN:
 #         change tracking and total changes
 #         =============================================================================
         self.m.addConstrs(w_initial_vars[well][dim] - inputs[well, sep, dim] <= changes[well, sep, dim]*w_initial_vars[well][dim]*w_relative_change[well][dim] for well in self.wellnames for sep in self.well_to_sep[well] for dim in range(self.multidims[well]["oil"][sep][0]))
+        
         self.m.addConstrs(inputs[well, sep, dim] - w_initial_vars[well][dim] <= changes[well, sep, dim]*w_initial_vars[well][dim]*w_relative_change[well][dim]+
                           (1-w_initial_prod[well])*w_max_lims[dim][well][sep]*changes[well, sep, dim] for well in self.wellnames for sep in self.well_to_sep[well] for dim in range(self.multidims[well]["oil"][sep][0]))
+        
         self.m.addConstr(quicksum(changes[well, sep, dim] for well in self.wellnames for sep in self.well_to_sep[well] for dim in range(self.multidims[well]["oil"][sep][0])) <= max_changes)
 
         # =============================================================================
@@ -442,6 +527,7 @@ class NN:
         
 
         self.m.optimize()
+        
         df = pd.DataFrame(columns=t.robust_res_columns) 
         chokes = [inputs[well, "HP", 0].x if outputs_gas[0, well, "HP"].x>0 else 0 for well in self.wellnames]
         rowlist=[]
